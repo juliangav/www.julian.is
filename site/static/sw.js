0824ed92a8a749556d258d01278c292177f6a545
Julian Gaviria <jgaviria@thomasnet.com>
2017-08-20 07:35:29 -0400

    Removed Google Tag Manager and added all scripts manually. This is too much overhead for what I'm tracking and was giving me issues with the scrolldepth events.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-v2v164';
+var CACHE_NAME = 'site-cache-v2v166';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['site-cache-v2v164'];
+  var cacheWhitelist = ['site-cache-v2v166'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
