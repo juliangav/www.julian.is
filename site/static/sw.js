302fa585ec1cf2bb0b2ccc9262d47e830479b200
Julian Gaviria <jgaviria@thomasnet.com>
2017-10-11 23:03:26 -0400

    Added constant sizing on padding in preparation for iPhone X.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-v2v166';
+var CACHE_NAME = 'site-cache-v2v167';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['site-cache-v2v166'];
+  var cacheWhitelist = ['site-cache-v2v167'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
