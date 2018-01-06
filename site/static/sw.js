61025dd680672cfbcfd539eb94c2df148f239bd0
Julian Gaviria <jgaviria@thomasnet.com>
2018-01-06 18:51:43 -0500

    Woopra Config.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-v2v182';
+var CACHE_NAME = 'site-cache-v2v183';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['site-cache-v2v182'];
+  var cacheWhitelist = ['site-cache-v2v183'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
