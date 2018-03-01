c803b6a6e6627ec5cc91040b3f521ae99911f405
Julian Gaviria <jgaviria@thomasnet.com>
2018-03-01 00:18:44 -0500

    Updated service workers version and removed Hotjar static code.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-v2v186';
+var CACHE_NAME = 'site-cache-v2v187';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['site-cache-v2v186'];
+  var cacheWhitelist = ['site-cache-v2v187'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
