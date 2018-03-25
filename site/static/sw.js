f8e53b4d09440a9969f83249e22714bcd26b1de5
Julian Gaviria <jgaviria@thomasnet.com>
2018-03-25 11:46:02 -0400

    Added Netlify branch variable.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-v2v195';
+var CACHE_NAME = 'site-cache-v2v196';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['site-cache-v2v195'];
+  var cacheWhitelist = ['site-cache-v2v196'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
