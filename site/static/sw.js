2694644257481a2d3680f38759be3c4c7e869224
Julian Gaviria <jgaviria@thomasnet.com>
2018-07-07 15:59:33 -0400

    Updated sw version.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-v2v198';
+var CACHE_NAME = 'site-cache-v2v200';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['site-cache-v2v198'];
+  var cacheWhitelist = ['site-cache-v2v200'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
