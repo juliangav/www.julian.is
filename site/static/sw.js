05a54a13e7487512779da8206fe93b97f9f5a7d8
Julian Gaviria <jgaviria@thomasnet.com>
2018-07-07 16:28:16 -0400

    Updated experiment id.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-v2v199';
+var CACHE_NAME = 'site-cache-v2v202';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['site-cache-v2v199'];
+  var cacheWhitelist = ['site-cache-v2v202'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
