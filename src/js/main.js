0ffbeb95c0734094584c09d6f222fdfa83f0091d
juliangav <hoolyon@hotmail.com>
2022-05-11 05:40:25 -0400

    Commented out exist intent popup.

--- src/js/main.js
+++ src/js/main.js
@@ -116,30 +116,30 @@ $(document).ready(function() {
 
 
 // Exit intent
-function addEvent(obj, evt, fn) {
-  if (obj.addEventListener) {
-    obj.addEventListener(evt, fn, false);
-  } else if (obj.attachEvent) {
-    obj.attachEvent("on" + evt, fn);
-  }
-}
+// function addEvent(obj, evt, fn) {
+//   if (obj.addEventListener) {
+//     obj.addEventListener(evt, fn, false);
+//   } else if (obj.attachEvent) {
+//     obj.attachEvent("on" + evt, fn);
+//   }
+// }
 
 // Exit intent trigger
-addEvent(document, 'mouseout', function(evt) {
+// addEvent(document, 'mouseout', function(evt) {
 
-  if (evt.toElement === null && evt.relatedTarget === null && !localStorage.getItem('exitintent_show')) {
-    $.magnificPopup.open({
-      items: {
-        src: '#newsletter-form' //ID of inline element
-      },
-      type: 'inline',
-      removalDelay: 500, //Delaying the removal in order to fit in the animation of the popup
-      mainClass: 'newsletter-popup mfp-fade mfp-fade-side', //The actual animation
-    });
+//   if (evt.toElement === null && evt.relatedTarget === null && !localStorage.getItem('exitintent_show')) {
+//     $.magnificPopup.open({
+//       items: {
+//         src: '#newsletter-form' //ID of inline element
+//       },
+//       type: 'inline',
+//       removalDelay: 500, //Delaying the removal in order to fit in the animation of the popup
+//       mainClass: 'newsletter-popup mfp-fade mfp-fade-side', //The actual animation
+//     });
 
-    localStorage.setItem('exitintent_show', 'true'); // Set the flag in localStorage
-  }
-});
+//     localStorage.setItem('exitintent_show', 'true'); // Set the flag in localStorage
+//   }
+// });
 
 
 //Delayed Popup with localstorage to show popup only once
