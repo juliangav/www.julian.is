1470f51c5388f898b30e912493e9643f99c6c348
juliangav <hoolyon@hotmail.com>
2022-05-20 06:52:05 -0400

    Updated SW cache.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-a159';
+var CACHE_NAME = 'site-cache-a1510';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-var cacheWhitelist = ['site-cache-a159'];
+var cacheWhitelist = ['site-cache-a1510'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
