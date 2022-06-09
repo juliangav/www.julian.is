ab39c42591bc6d696491fa236ad03d4210069dc9
juliangav <hoolyon@hotmail.com>
2022-06-09 13:49:00 -0400

    Created Boundless cover letter.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-a1513';
+var CACHE_NAME = 'site-cache-a1514';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-var cacheWhitelist = ['site-cache-a1513'];
+var cacheWhitelist = ['site-cache-a1514'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
