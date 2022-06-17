78111c4757f6f8bd287415c63299ec7a6a8fa1d7
juliangav <hoolyon@hotmail.com>
2022-06-17 14:07:08 -0400

    Updated sw.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-a1517';
+var CACHE_NAME = 'site-cache-a1518';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-var cacheWhitelist = ['site-cache-a1517'];
+var cacheWhitelist = ['site-cache-a1518'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
