874d50fc07d9db89289c59057847d0135125205f
juliangav <hoolyon@hotmail.com>
2022-08-15 21:24:00 -0400

    Included contact info.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-a1530';
+var CACHE_NAME = 'site-cache-a1531';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-var cacheWhitelist = ['site-cache-a1530'];
+var cacheWhitelist = ['site-cache-a1531'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
