eae432e8d77dd6b779e31b673636050f0e6a862b
juliangav <hoolyon@hotmail.com>
2022-08-16 14:22:05 -0400

    Replaced list page.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-a1536';
+var CACHE_NAME = 'site-cache-a1537';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-var cacheWhitelist = ['site-cache-a1536'];
+var cacheWhitelist = ['site-cache-a1537'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
