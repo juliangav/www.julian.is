a20432155d056ef6d0d132c47702a9d9692ce30e
juliangav <hoolyon@hotmail.com>
2022-08-16 11:24:04 -0400

    Created cl for kan.

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'site-cache-a1531';
+var CACHE_NAME = 'site-cache-a1532';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -58,7 +58,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-var cacheWhitelist = ['site-cache-a1531'];
+var cacheWhitelist = ['site-cache-a1532'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
