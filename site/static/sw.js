56cba83f00d4d3f843ad598a43577abd10a2c784
juliangav <hoolyon@hotmail.com>
2026-06-04 19:13:48 -0400

    Reset service worker cache on deploy

--- site/static/sw.js
+++ site/static/sw.js
@@ -1,4 +1,5 @@
-var CACHE_NAME = 'site-cache-a1542';
+var CACHE_PREFIX = 'site-cache-';
+var CACHE_NAME = 'site-cache-20260604-reset';
 var urlsToCache = [
   '/',
   '/style.css',
@@ -6,50 +7,71 @@ var urlsToCache = [
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
     .then(function(cache) {
       console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
+    .then(function() {
+      return self.skipWaiting();
+    })
   );
 });
 
+function isHtmlRequest(request) {
+  return request.mode === 'navigate' ||
+    (request.headers.get('accept') || '').indexOf('text/html') !== -1;
+}
+
+function cacheResponse(request, response) {
+  if (!response || response.status !== 200 || response.type !== 'basic') {
+    return response;
+  }
+
+  var responseToCache = response.clone();
+
+  caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(request, responseToCache);
+    });
+
+  return response;
+}
+
 self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  if (isHtmlRequest(event.request)) {
+    event.respondWith(
+      fetch(event.request)
+      .then(function(response) {
+        return cacheResponse(event.request, response);
+      })
+      .catch(function() {
+        return caches.match(event.request)
+          .then(function(response) {
+            return response || caches.match('/');
+          });
+      })
+    );
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request)
     .then(function(response) {
-      // Cache hit - return response
       if (response) {
         return response;
       }
 
-      // IMPORTANT: Clone the request. A request is a stream and
-      // can only be consumed once. Since we are consuming this
-      // once by cache and once by the browser for fetch, we need
-      // to clone the response.
       var fetchRequest = event.request.clone();
 
       return fetch(fetchRequest).then(
         function(response) {
-          // Check if we received a valid response
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-
-          // IMPORTANT: Clone the response. A response is a stream
-          // and because we want the browser to consume the response
-          // as well as the cache consuming the response, we need
-          // to clone it so we have two streams.
-          var responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then(function(cache) {
-              cache.put(event.request, responseToCache);
-            });
-
-          return response;
+          return cacheResponse(event.request, response);
         }
       );
     })
@@ -57,18 +79,26 @@ self.addEventListener('fetch', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-
-var cacheWhitelist = ['site-cache-a1542'];
-
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (
+            cacheName !== CACHE_NAME &&
+            (cacheName.indexOf(CACHE_PREFIX) === 0 || cacheName === 'site-cache')
+          ) {
             return caches.delete(cacheName);
           }
         })
       );
+    }).then(function() {
+      return self.clients.claim();
     })
   );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
